fe4c35d4848cd53397984a1dd30bfafe559ebc68
quakig <bha100710@gmail.com>
2015-03-13 12:50:29 +0530

    fixed minor bug in media selectors + added support for child selectors

--- tests.php
+++ tests.php
@@ -34,7 +34,7 @@ foreach($unused as $unuseditem) {
 		$pieces = explode(' ', $unuseditem);
 		//echo $unuseditem.'----'.$pieces[0]."<br>";
 		$pieces[0] = preg_quote($pieces[0], '/'); 
-		$cssfile = preg_replace('/(?:(?<=^|\s)(?=\S|$)|(?<=^|\S)(?=\s|$))'.$pieces[0].' +.+[^,{]{/', "{", $cssfile);	
+		$cssfile = preg_replace('/(?:(?<=^|\s)(?=\S|$)|(?<=^|\S)(?=\s|$))'.$pieces[0].'( |>)+.+[^,{]{/', "{", $cssfile);	
 		$unused = array_diff($unused, array($unuseditem));	
 	}
 
@@ -51,7 +51,7 @@ foreach($unused as $unuseditem) {
 
 
 $cssfile = preg_replace("/(,\s*){2,}/", ",", $cssfile);  // remove multiple instances of comma
-$cssfile = preg_replace("/}\s*?,/", "}", $cssfile); // remove deinitions with only comma left as selector
+$cssfile = preg_replace("/}\s*?(,|>)/", "}", $cssfile); // remove deinitions with only comma or > left as selector
 
 do {
     $cssfile = preg_replace('/}\s*,?\s*{[^}]*}/S', "}", $cssfile, -1, $count); //remove definitions with no selector elements
@@ -59,7 +59,7 @@ do {
 
 
 do {
-    $cssfile = preg_replace('/{\s*,?\s*{[^}]*}/S', "}", $cssfile, -1, $count);// handle 1st unused definition within media query format like ' { {some definitions here }'
+    $cssfile = preg_replace('/{\s*,?\s*{[^}]*}/S', "{", $cssfile, -1, $count);// handle 1st unused definition within media query format like ' { {some definitions here }'
 } while ($count);
 
 
